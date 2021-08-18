6e4363f9c0774b7f4bac77b1e24115c5b33ddcae
AnaBeaa <anybeatrizsouza@gmail.com>
2021-08-17 23:39:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-e7y27n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-e7y27n)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h5 style={{color:"#033E3e", textAlign: "center"}}>meu nome é bea</h5>
+    </div>
+  );
+}
